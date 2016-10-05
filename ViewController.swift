5b86f0e4c06f4bede183388e8334f631121a69a8
zackwitz <zackwitz@gmail.com>
2016-10-05 10:32:59 -0700

    Created stuff

--- ViewController.swift
+++ ViewController.swift
@@ -10,9 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var oneButton: UIButton!
+    @IBOutlet weak var twoButton: UIButton!
+    @IBOutlet weak var threeButton: UIButton!
+    @IBOutlet weak var fourButton: UIButton!
+    @IBOutlet weak var fiveButton: UIButton!
+    @IBOutlet weak var sixButton: UIButton!
+    @IBOutlet weak var sevenButton: UIButton!
+    @IBOutlet weak var eightButton: UIButton!
+    @IBOutlet weak var nineButton: UIButton!
+    @IBOutlet weak var zeroButton: UIButton!
+    @IBOutlet weak var decimalButton: UIButton!
+    @IBOutlet weak var negativeButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var timesButton: UIButton!
+    @IBOutlet weak var divideButton: UIButton!
+    @IBOutlet weak var clearButton: UIButton!
+    @IBOutlet weak var enterButton: UIButton!
+    @IBOutlet weak var answer: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+      
+        
     }
 
     override func didReceiveMemoryWarning() {
